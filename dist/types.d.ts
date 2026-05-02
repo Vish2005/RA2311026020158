891acad8d4d48bccfd882207724b33d684e2ad6b
Vishaka <vishakakvk@gmail.com>
2026-05-02 12:21:51 +0530

    Add .gitignore and remove node_modules

--- dist/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type Stack = "backend" | "frontend";
-export type Level = "debug" | "info" | "warn" | "error" | "fatal";
-export type Package = "api" | "component" | "hook" | "page" | "state" | "style" | "cache" | "controller" | "cron_job" | "db" | "domain" | "handler" | "repository" | "route" | "service" | "auth" | "config" | "middleware" | "utils";
-export interface LogPayload {
-    stack: Stack;
-    level: Level;
-    package: Package;
-    message: string;
-}
